9e2328c5f68e062d96ffcffac328c85292ec7826
david <david.segalabutla@nlsa.ac.za>
2012-03-04 05:29:07 +0200

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dave::Application.config.secret_token = '78daed2986d2899c86fd4b80794dc6a67196d53d5867ec39b140d9e30c7df4333d990c76126524cc69149eea683ff15ddb514f8720eaee9e13952ed036de60b7'
